cd9530a8c06e4b70de8548da3e9bed5b27af0cb5
CryLegend <crylegend95@gmail.com>
2014-01-02 12:55:31 +0100

    Ajout du support multiserveur

--- BungeeAuthMeBridge/src/com/crylegend/bungeeauthmebridge/BungeeAuthMeBridge.java
+++ BungeeAuthMeBridge/src/com/crylegend/bungeeauthmebridge/BungeeAuthMeBridge.java
@@ -1,14 +1,15 @@
 package com.crylegend.bungeeauthmebridge;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class BungeeAuthMeBridge extends Plugin{
-	
-	public List<String> authList = new ArrayList<String>();
+    
+	public HashMap<String,LinkedList<String>> authList = new HashMap<String,LinkedList<String>>();
 
 	public void onEnable() {
 		ProxyServer.getInstance().getPluginManager().registerListener(this, new BungeeAuthMeBridgeListener(this));
