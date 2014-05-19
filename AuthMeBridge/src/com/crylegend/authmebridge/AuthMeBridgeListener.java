8506322ce2679674c473206304d56830fd5b3c50
Anis <crylegend95@gmail.com>
2014-05-19 08:19:17 +0200

    Ajout du support pour les sessions

--- AuthMeBridge/src/com/crylegend/authmebridge/AuthMeBridgeListener.java
+++ AuthMeBridge/src/com/crylegend/authmebridge/AuthMeBridgeListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import fr.xephi.authme.events.LoginEvent;
+import fr.xephi.authme.events.SessionEvent;
 
 public class AuthMeBridgeListener implements Listener{
 	AuthMeBridge plugin;
@@ -21,13 +22,23 @@ public class AuthMeBridgeListener implements Listener{
 	public void onAuthMeLogin(LoginEvent event) {
 		if (!event.isLogin())
 			return;
-		final Player player = event.getPlayer();
+		playerLogin(event.getPlayer());
+	}
+
+	@EventHandler
+	public void onAuthMeSession(SessionEvent event) {
+		if (!event.isLogin())
+			return;
+		playerLogin(plugin.getServer().getPlayer(event.getPlayerAuth().getNickname()));
+	}
+	
+	public void playerLogin(final Player player) {
 		ByteArrayOutputStream b = new ByteArrayOutputStream();
 		DataOutputStream out = new DataOutputStream(b);
 		try {
 			out.writeUTF("PlayerLogin");
 			out.writeUTF(player.getUniqueId().toString());
-			new PluginMessageTask(plugin, player, b).runTaskAsynchronously(plugin);
+			new PluginMessageTask(plugin, b).runTaskAsynchronously(plugin);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
