2881919bc12030cf708f43c4076cd085caddbf73
Anis <crylegend95@gmail.com>
2014-05-19 08:22:21 +0200

    Suppression d'anciennes et ajout de nouvelles options: désormais, on peut utiliser le plugin sans avoir tous ses serveurs sous AuthMeBridge.

--- BungeeAuthMeBridge/src/com/crylegend/bungeeauthmebridge/BungeeAuthMeBridge.java
+++ BungeeAuthMeBridge/src/com/crylegend/bungeeauthmebridge/BungeeAuthMeBridge.java
@@ -2,27 +2,22 @@ package com.crylegend.bungeeauthmebridge;
 
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map.Entry;
+import java.util.List;
 import java.util.UUID;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import net.md_5.bungee.api.ProxyServer;
 import net.craftminecraft.bungee.bungeeyaml.pluginapi.ConfigurablePlugin;
 
 public class BungeeAuthMeBridge extends ConfigurablePlugin{
 	String incomingChannel = "AuthMeBridge";
-	String outcomingChannel = "BAuthMeBridge";
-	String mainServer;
+	List<String> serversList;
+	List<String> commandsWhitelist;
 	HashMap<String,LinkedList<UUID>> authList = new HashMap<String,LinkedList<UUID>>();
 	boolean serverSwitchRequiresAuth;
 
 	public void onEnable() {
 	    saveDefaultConfig();
-	    mainServer = getConfig().getString("mainServer");
+	    serversList = getConfig().getStringList("serversList");
+	    commandsWhitelist = getConfig().getStringList("commandsWhitelist");
 	    serverSwitchRequiresAuth = getConfig().getBoolean("serverSwitchRequiresAuth");
 	    getProxy().registerChannel(incomingChannel);
 		getProxy().getPluginManager().registerListener(this, new BungeeAuthMeBridgeListener(this));
--- BungeeAuthMeBridge/src/com/crylegend/bungeeauthmebridge/BungeeAuthMeBridgeListener.java
+++ BungeeAuthMeBridge/src/com/crylegend/bungeeauthmebridge/BungeeAuthMeBridgeListener.java
@@ -5,14 +5,11 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.ChatEvent;
-import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
@@ -25,7 +22,7 @@ public class BungeeAuthMeBridgeListener implements Listener{
 	public BungeeAuthMeBridgeListener(BungeeAuthMeBridge plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@EventHandler
 	public void onChat(ChatEvent event) {
 		if (!(event.getSender() instanceof ProxiedPlayer))
@@ -35,8 +32,12 @@ public class BungeeAuthMeBridgeListener implements Listener{
 			return;
 		ProxiedPlayer player = (ProxiedPlayer)event.getSender();
 		String server = player.getServer().getInfo().getName();
-	    if (!plugin.authList.containsKey(server))
-	    	plugin.authList.put(server, new LinkedList<UUID>());
+		if (!plugin.serversList.contains(server))
+			return;
+		if (plugin.commandsWhitelist.contains(cmd))
+			return;
+		if (!plugin.authList.containsKey(server))
+			plugin.authList.put(server, new LinkedList<UUID>());
 		if (plugin.authList.get(server).isEmpty() || !plugin.authList.get(server).contains(player.getUniqueId()))
 			event.setCancelled(true);
 	}
@@ -49,40 +50,50 @@ public class BungeeAuthMeBridgeListener implements Listener{
 			return;
 		event.setCancelled(true);
 		String server = ((Server) event.getSender()).getInfo().getName();
-	    DataInputStream in = new DataInputStream(new ByteArrayInputStream(event.getData()));
-	    String task = in.readUTF();
-	    if (!task.equals("PlayerLogin"))
-	    	return;
-	    event.setCancelled(true);
-	    if (!plugin.authList.containsKey(server))
-	    	plugin.authList.put(server, new LinkedList<UUID>());
-	    UUID uuid = UUID.fromString(in.readUTF());
-	    plugin.authList.get(server).add(uuid);
+		DataInputStream in = new DataInputStream(new ByteArrayInputStream(event.getData()));
+		String task = in.readUTF();
+		if (!task.equals("PlayerLogin"))
+			return;
+		event.setCancelled(true);
+		if (!plugin.authList.containsKey(server))
+			plugin.authList.put(server, new LinkedList<UUID>());
+		UUID uuid = UUID.fromString(in.readUTF());
+		plugin.authList.get(server).add(uuid);
 	}
 
 	@EventHandler
 	public void onLeave(PlayerDisconnectEvent event) {
 		UUID uuid = event.getPlayer().getUniqueId();
-		if (event.getPlayer().getServer() == null)
-			return;
-		String server = event.getPlayer().getServer().getInfo().getName();
-	    if (plugin.authList.containsKey(server))
-	    	if (plugin.authList.get(server).contains(uuid))
-	    		plugin.authList.get(server).remove(uuid);
+		if (event.getPlayer().getServer() == null) {
+			for (LinkedList<UUID> list: plugin.authList.values())
+				if (list.contains(uuid))
+					list.remove(uuid);
+		}
+		else {
+			String server = event.getPlayer().getServer().getInfo().getName();
+			if (plugin.authList.containsKey(server))
+				if (plugin.authList.get(server).contains(uuid))
+					plugin.authList.get(server).remove(uuid);
+		}
 	}
-	
+
 	@EventHandler
 	public void onServerSwitch(ServerSwitchEvent event) {
+		UUID uuid = event.getPlayer().getUniqueId();
+		String server = event.getPlayer().getServer().getInfo().getName();
+		if (plugin.authList.containsKey(server))
+			if (plugin.authList.get(server).contains(uuid))
+				plugin.authList.get(server).remove(uuid);
 		if (!plugin.serverSwitchRequiresAuth)
 			return;
 		ProxiedPlayer player = event.getPlayer();
-	    for (LinkedList<UUID> list: plugin.authList.values())
-	    	if (list.contains(player.getUniqueId()))
-	    		return;
-	    if (player.getServer().getInfo().getName().equalsIgnoreCase(plugin.mainServer))
-	    	return;
-	    TextComponent kickReason = new TextComponent("Authentification requise.");
-	    kickReason.setColor(ChatColor.RED);
-	    player.disconnect(kickReason);
+		for (LinkedList<UUID> list: plugin.authList.values())
+			if (list.contains(player.getUniqueId()))
+				return;
+		if (server.equalsIgnoreCase(player.getPendingConnection().getListener().getDefaultServer()))
+			return;
+		TextComponent kickReason = new TextComponent("Authentication required.");
+		kickReason.setColor(ChatColor.RED);
+		player.disconnect(kickReason);
 	}
 }
